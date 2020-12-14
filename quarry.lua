568ea8229cafcf2b139f1df3a6f39606f409d3e0
Alex Boggs <catacorn1@gmail.com>
2020-12-14 15:53:47 -0500

    cum

--- quarry.lua
+++ quarry.lua
@@ -1,4 +1,3 @@
-term.clear()
 io.write("girth: ")
 girth = io.read()
 io.write("length: ")
@@ -8,9 +7,9 @@ depth = io.read()
 io.write("yeet? ")
 yeet = io.read()
 
-x = 0
-y = 0
-z = 0
+posX = 0
+posY = 0
+posZ = 0
 
 rotation = 0
 
@@ -27,21 +26,118 @@ function rotate()
 	end
 end
 
-function digStraight()
-	turtle.digDown()
-	turtle.dig()
-	turtle.dig()
-	turtle.forward()
-	if rotation == 0 then
-		z = z + 1
-	elseif rotation == 1 then
-		x = x + 1
-	elseif rotation == 2 then
-		z = z - 1
-	elseif rotation == 3 then
-		x = x - 1
+function goHome()
+	rotate()
+	local step = 0
+	for step = y - 1, 0, -1 do
+		turtle.up()
 	end
-	turtle.digUp()
+	for step = x - 1, 0, -1 do
+		turtle.forward()
+	end
+	turtle.turnLeft()
+	for step = z - 1, 0, -1 do
+		turtle.forward()
+	end
+end
+
+function resume()
+	turtle.turnLeft()
+	turtle.turnLeft()
+	local step = 0
+	for step = 0, z - 1, 1 do
+		turtle.forward()
+	end
+	turtle.turnRight()
+	for step = 0, x - 1, 1 do
+		turtle.forward()
+	end
+	for step = 0, y - 1, 1 do
+		turtle.down()
+	end
+	if rotation == 0 then
+		turtle.turnLeft()
+	elseif rotation == 2 then
+		turtle.turnRight()
+	elseif rotation == 3 then
+		turtle.turnRight()
+		turtle.turnRight()
+	end
+end
+
+function checkFuel()
+	turtle.select(1)
+	turtle.refuel()
+	if turtle.getFuelLevel() <= x + y + z + 1 then
+		refill = 1
+		empty()
+		refill = 0
+	end
+end
+
+function empty()
+	recover()
+	local search = 0
+	for search = 16, 1, -1 do
+		turtle.select(search)
+		turtle.drop()
+	end
+	if refill == 1 then
+		turtle.turnRight()
+		while turtle.getFuelLevel() <= x + y + z + 1 do
+			if turtle.suck() == true then
+				turtle.suck()
+				turtle.select(1)
+				turtle.refuel()
+			elseif turtle.suck() == false then
+				turtle.select(1)
+				turtle.refuel()
+				term.clear()
+				term.setCursorPos(1,1)
+				io.write("out of fuel")
+			end
+		end
+		turtle.turnLeft()
+		resume()
+	end
+	if done ~= 1 then
+		resume()
+	end
+end
+
+function checkFull()
+	fullSlots = 0
+	local search = 0
+	for search = 16, 1, -1 do
+		turtle.select(search)
+		if turtle.getItemCount() > 0 then
+			if yeet == "yes" then
+				if turtle.getItemDetail().name == "minecraft:cobblestone" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "minecraft:stone" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "minecraft:dirt" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "minecraft:gravel" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "chisel:marble2" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "chisel:limestone2" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "minecraft:netherrack" then
+					turtle.drop()
+				elseif turtle.getItemDetail().name == "natura:nether_tainted_soil" then
+					turtle.drop()
+				end
+			end
+		end
+		if turtle.getItemCount() > 0 then
+			fullSlots = fullSlots + 1
+		end
+	end
+	if fullSlots == 16 then
+		empty()
+	end
 end
 
 function nextRow()
@@ -77,6 +173,7 @@ function nextRow()
 end
 
 function digDown()
+	checkFuel()
 	local step = 0
 	for step = 2, 0, -1 do
 		turtle.digDown()
@@ -86,10 +183,29 @@ function digDown()
 	end
 end
 
+function digStraight()
+	checkFuel()
+	turtle.digDown()
+	turtle.dig()
+	turtle.dig()
+	turtle.forward()
+	if rotation == 0 then
+		z = z + 1
+	elseif rotation == 1 then
+		x = x + 1
+	elseif rotation == 2 then
+		z = z - 1
+	elseif rotation == 3 then
+		x = x - 1
+	end
+	turtle.digUp()
+end
+
 function quarry()
 	turn = 0
 	done = 0
 	depth = tonumber(depth)
+	checkFuel()
 	turtle.digUp()
 	turtle.up()
 	y = y - 1
@@ -99,6 +215,7 @@ function quarry()
 			for r = girth, 2, -1 do
 				digStraight()
 			end
+			checkFull()
 			if c == 1 then
 				turtle.turnRight()
 				turtle.turnRight()
@@ -123,6 +240,7 @@ function quarry()
 	end
 	turtle.digDown()
 	done = 1
+	empty()
 end
 
 quarry()
